4e468e4c1e588ea320c85095455c4760997e65bc
Juan M. Payán Barea <89635948+jpaybar@users.noreply.github.com>
2022-09-26 12:18:07 +0200

    Adding cleaning the system
    Adding cleaning the system

--- script_tools/install_guest_additions.sh
+++ script_tools/install_guest_additions.sh
@@ -47,43 +47,62 @@ echo
 echo "Press intro to run the script"
 read intro
 
+# Updating repositories
 echo -e "${GREEN}Updating repositories ....${NC}"
 apt-get update > /dev/null 2>&1
 echo
 
+# Installing kernel headers and developer tools
 echo -e "${ORANGE}Installing the linux kernel headers and the basic developer tools ....${NC}"
 apt-get install linux-headers-$(uname -r) build-essential dkms -y > /dev/null 2>&1
 echo
 
+# Downloading the latest Guest Additions version
 echo -e "${GREEN}Downloading the latest version \"${VERSION}\" of VirtualBox's Guest Additions ....${NC}"
 echo
 wget http://download.virtualbox.org/virtualbox/${VERSION}/VBoxGuestAdditions_${VERSION}.iso
 
+# Creating mount point
 mkdir /media/VBoxGuestAdditions
 echo -e "${ORANGE}Creating \"/media/VBoxGuestAdditions\" directory ....${NC}"
 echo
 
+# Mounting iso files
 mount -o loop,ro VBoxGuestAdditions_${VERSION}.iso /media/VBoxGuestAdditions
 echo -e "${GREEN}Mounting \"VBoxGuestAdditions_${VERSION}.iso\" on \"/media/VBoxGuestAdditions\" directory ....${NC}"
 echo
 
+# Running guest additions
 echo -e "${ORANGE}Running \"VBoxLinuxAdditions.run\" ....${NC}"
 sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run > /dev/null 2>&1
 echo
 
+# Removing iso file an version
 rm VBoxGuestAdditions_${VERSION}.iso LATEST-STABLE.TXT
 echo -e "${GREEN}Removing files ....${NC}"
 echo
 
+# Umounting folder
 umount /media/VBoxGuestAdditions
 echo -e "${ORANGE}Umounting \"/media/VBoxGuestAdditions\" directory ....${NC}"
 echo
 
+# Removing mount point
 rmdir /media/VBoxGuestAdditions
 echo -e "${GREEN}Deleting mount point directory ....${NC}"
 echo
 
+# Checking installation of the guest additions
 echo -e "${ORANGE}Checking \"Guest Additions\" setup ....${NC}"
 echo
 lsmod | grep -i vbox
 echo
+
+# Clean system to minimize size
+echo -e "${GREEN}Cleaning the system to minimize the size of the \".box\" file ....${NC}"
+echo
+apt-get autoremove > /dev/null 2>&1
+apt-get clean
+dd if=/dev/zero of=/EMPTY bs=1M > /dev/null 2>&1
+rm -f /EMPTY
+cat /dev/null > ~/.bash_history && history -c && exit
